6e4b73cbc3c56b84fb78cc8f809eeb1a0e4f345f
Danh Vo <danh.vo.2110@gmail.com>
2023-10-31 02:33:21 +0700

    update lần 1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 closeBoxUpdate = () => {
     const modalBoxes = document.getElementsByClassName("modal_box")
     if(modalBoxes.length > 0){
@@ -5,29 +6,8 @@ closeBoxUpdate = () => {
     }
 }   
 
-openBoxUpdate = () => {
-    const modalBoxes = document.getElementsByClassName("modal_box")
-    if(modalBoxes.length > 0){
-        modalBoxes[0].style.display = "flex"
-    }
-}
+
 // update ảnh bìa + nền
-updateImage = () => {
-    const fileInput = document.getElementById("fileInput");
-    fileInput.click();
-    fileInput.addEventListener("change", function() {
-        const file = fileInput.files[0];
-        if (file) {
-            const reader = new FileReader();
-
-            reader.onload = function (e) {
-                displayImage.src = e.target.result;
-            };
-
-            reader.readAsDataURL(file);
-        }
-    })
-}
 
 updateMainImage =() => {
     const fileInput = document.getElementById("fileMainInput");
@@ -45,3 +25,5 @@ updateMainImage =() => {
         }
     })
 }
+
+
